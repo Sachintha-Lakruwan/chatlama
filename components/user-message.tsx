557812257d64ae42c88d62c750c908356f41a2b1
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2025-08-04 18:36:42 +0530

    bug fix: multiline showing on same line

--- components/user-message.tsx
+++ components/user-message.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function UserMessage({ content }: { content: string }) {
   return (
     <div className=" w-fit max-w-4/5 p-4 bg-default-200 rounded-3xl ml-auto">
-      <p>{content}</p>
+      <p className="whitespace-pre-wrap">{content}</p>
     </div>
   );
 }
